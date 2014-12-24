7e062b94999c045cd717b396fd78290d161b4582
Taryn Hill <Phrohdoh@gmail.com>
2014-12-23 21:21:06 -0600

    Tabs -> Spaces Right-clicking a brick destroys it and drops its powerdrop if it exist. Powerdrop images must be named `powerdrop_<name>.png`.

--- pyball.py
+++ pyball.py
@@ -16,56 +16,114 @@ brick_image = pygame.image.load('brick.png')
 brick_image_size = brick_image.get_rect().size
 
 bricks = []
+powerdrops = []
 
-class Powerdrop(Enum):
-	Nothing = 0
-	LoseLife = 1
-	GainLife = 2
+class PowerdropEnum(Enum):
+    Nothing = 0
+    LoseLife = 1
+    GainLife = 2
+
+class MouseButton(Enum):
+    Left = 1
+    Scroll = 2
+    Right = 3
+
+class PowerDrop:
+    def __init__(self, powerdrop, (x, y)):
+        self.droptype = powerdrop
+        self.image = pygame.image.load('powerdrop_%s.png' %(powerdrop.name.lower()))
+        self.id = len(powerdrops) + 1
+        self.x = x
+        self.y = y
+
+    def destroy(self):
+        print "Destroying powerdrop %s; %s." %(self.id, self.droptype.name)
+        powerdrops.remove(self)
+
+    def tick(self):
+        if self.y > height:
+            self.destroy()
+
+        self.y += 2
 
 class Brick:
-	def __init__(self, powerdrop, (x, y)):
-		self.image = pygame.image.load('brick.png')
-		self.powerdrop = powerdrop
-		self.id = len(bricks) + 1
-		self.x = x
-		self.y = y
-		self.haspower = powerdrop != Powerdrop.Nothing
+    def __init__(self, powerdrop, (x, y)):
+        self.image = pygame.image.load('brick.png')
+        self.powerdrop = powerdrop
+        self.id = len(bricks) + 1
+        self.x = x
+        self.y = y
+        self.width = self.image.get_rect().size[0]
+        self.height = self.image.get_rect().size[1]
+        self.haspower = powerdrop != PowerdropEnum.Nothing
 
-	def drop_power():
-		pass
+    def bounds(self):
+        x = self.x
+        y = self.y
+        return (x, x + self.width, y, y + self.height)
+
+    def destroy(self):
+        print "Destroying brick %s." %(self.id)
+        if self.haspower:
+            power = PowerDrop(self.powerdrop, (self.x, self.y + 20))
+            powerdrops.append(power)
+
+        bricks.remove(self)
+
+def brick_at_pos(x, y):
+    for b in bricks:
+        x1, x2, y1, y2 = b.bounds()
+        if x1 < x <= x2:
+            if y1 < y <= y2:
+                return b
+
+    return None
 
 def tick():
-	while True:
-		for e in pygame.event.get():
-			etype = e.type
+    while True:
+        for e in pygame.event.get():
+            etype = e.type
 
-			if etype == pygame.QUIT:
-				return
+            if etype == pygame.QUIT:
+                return
 
-		display.fill(black)
+            if etype == pygame.MOUSEBUTTONDOWN:
+                mx, my = e.pos
 
-		for b in bricks:
-			draw_image(b.image, (b.x, b.y))
+                if e.button == MouseButton.Right.value:
+                    clicked = brick_at_pos(mx, my)
 
-		pygame.display.update()
-		clock.tick(60)
+                    if clicked is not None:
+                        clicked.destroy()
+
+        display.fill(black)
+
+        for b in bricks:
+            draw_image(b.image, (b.x, b.y))
+
+        for p in powerdrops:
+            draw_image(p.image, (p.x, p.y))
+            p.tick()
+
+        pygame.display.update()
+        clock.tick(60)
 
 def draw_image(image, (x, y)):
-	display.blit(image, (x, y))
+    display.blit(image, (x, y))
 
 def init():
-	bricks_per_row = width / brick_image_size[0]
+    bricks_per_row = width / brick_image_size[0]
 
-	for row in range(0, 5):
-		for col in range(0, bricks_per_row):
-			power = random.choice(list(Powerdrop))
-			b = Brick(power, (col * brick_image_size[0], row * brick_image_size[1]))
-			bricks.append(b)
+    for row in range(5):
+        for col in range(bricks_per_row):
+            power = random.choice(list(PowerdropEnum))
+            b = Brick(power, (col * brick_image_size[0], row * brick_image_size[1]))
+            bricks.append(b)
 
-	# Visual test
-	for b in bricks:
-		if not b.haspower:
-			bricks.remove(b)
+    # Visual test
+    # for b in bricks:
+    # 	if not b.haspower:
+    # 		bricks.remove(b)
 
 init()
 tick()
